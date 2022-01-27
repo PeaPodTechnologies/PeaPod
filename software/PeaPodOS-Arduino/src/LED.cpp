523cf7a992462e7f35d58b2d024ab84641377554
Jayden Lefebvre <jayden.lefebvre55@gmail.com>
2022-01-27 18:28:43 -0500

    LED Class Improvements
    led color enum, parsing to String

--- software/PeaPodOS-Arduino/src/LED.cpp
+++ software/PeaPodOS-Arduino/src/LED.cpp
@@ -1,7 +1,24 @@
 #include "LED.h"
 
-LED::LED(uint8_t pin) : Actuator("LED", "led0", "led"){
+LED::LED(uint8_t pin, led_t color) : Actuator(parseLED(color), String("CREE" + parseLED(color)), "Lighting"){
     this->pin = pin;
+}
+
+String parseLED(led_t led){
+  switch(led){
+      case LED_BLUE:
+        return String("Royal Blue LED");
+      case LED_COOL:
+        return String("Cool White LED");
+      case LED_WARM:
+        return String("Warm White LED");
+      case LED_RED:
+        return String("Photo Red LED");
+      case LED_FAR:
+        return String("Far Red LED");
+      default:
+        return String("Unknown LED");
+  }
 }
 
 bool LED::init(){
@@ -13,6 +30,6 @@ bool LED::init(){
 void LED::update(){
     // Clamp to 0<x<1
     this->target = min(max(target, 0), 1);
-    Serial.println(this->target*255*LED_BRIGHTNESS_FACTOR);
-    analogWrite(this->pin, this->target*255*LED_BRIGHTNESS_FACTOR);
+    Serial.println(this->target*255);
+    analogWrite(this->pin, this->target*255);
 }
--- software/PeaPodOS-Arduino/src/LED.h
+++ software/PeaPodOS-Arduino/src/LED.h
@@ -3,6 +3,14 @@
 
 #include "Actuator.h"
 
+enum led_t {
+  LED_BLUE,
+  LED_COOL,
+  LED_WARM,
+  LED_RED,
+  LED_FAR
+};
+
 class LED : public Actuator {
     public:
         LED(uint8_t pin);
